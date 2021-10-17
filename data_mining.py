743fe70a193b124d33f7ffc7affb74c6acb44ff7
AK49001 <85910946+AK49001@users.noreply.github.com>
2021-10-16 23:16:05 -0400

    Add files via upload

---
+++ data_mining.py
@@ -0,0 +1,23 @@
+from typing import List,TextIO
+
+#file = open('HTV_DATA.csv')    #sample data file can be changed
+
+def csv_to_list(csv_file: TextIO) -> List[List[str]]:
+
+    csv_file.readline()
+
+    data = []
+    for line in csv_file:
+        data.append(line.strip().split(','))
+    return data
+
+def give_recommended_product(product_code: int, csv_file: TextIO) -> List[int]:
+    data = csv_to_list(csv_file)
+    Recommendation = []
+    for i in range(len(data)):
+        if int(data[i][0]) == product_code:
+            for j in range(1, len(data[i])):
+                #print("hiii")
+                if data[i][j] != '' and int(data[i][j]) != product_code:
+                    Recommendation.append(int(data[i][j]))
+    return Recommendation
